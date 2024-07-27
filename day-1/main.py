63e391ed90e3ccc965616b97b25681dedac9f6f0
Mahmood <mahmood.kn1526@gmail.com>
2024-07-27 20:57:01 +0330

    variables and input

--- day-1/main.py
+++ day-1/main.py
@@ -1,2 +1,22 @@
 # python print function
-print('hello world\nhello world'+' Mahmood')
+# print('hello world\nhello world'+' Mahmood')
+# get data from user
+# 13. x= input("Whats your name?")
+# print(len(x))
+# 14. challenge
+# a= input('a:')
+# b= input('b:')
+
+# c=a
+# a=b
+# b=c
+
+# print('a= '+a+'\n'+'b= '+b)
+
+# 16. day 1 project
+print("Welcome to the Band Name Generator.")
+city=input("What's name of the city you grew up in?\n")
+pet= input("What's your pet's name?\n")
+print("Your band name could be "+city+" "+pet)
+
+
