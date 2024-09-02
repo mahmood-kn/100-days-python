19b36d8d4c598b5ff78c3fcab2a2e26a3655108b
Mahmood <mahmood.kn1526@gmail.com>
2024-09-02 12:31:20 +0330

    complete challenge 2 day 8

--- day-8/main.py
+++ day-8/main.py
@@ -2,13 +2,33 @@ import math
 
 # exercise 1
 
-test_h= int(input("Height of wall: "))
-test_w= int(input("Width of wall: "))
-coverage=5
+# test_h= int(input("Height of wall: "))
+# test_w= int(input("Width of wall: "))
+# coverage=5
 
-def paint_calc(height,width,cover):
-  return math.ceil((height*width)/cover)
+# def paint_calc(height,width,cover):
+#   return math.ceil((height*width)/cover)
 
-cans=paint_calc(height=test_h,width=test_w,cover=coverage)
+# cans=paint_calc(height=test_h,width=test_w,cover=coverage)
 
-print(f"You'll need {cans} cans of paint.")
+# print(f"You'll need {cans} cans of paint.")
+
+# exercise 2 (prime number checker)
+n=int(input('Check this number: '))
+
+def prime_checker(number):
+  is_prime=True
+  if number== 1:
+    print('1 is neither prime nor composite number.')
+  else:
+    for i in range(2,math.floor(number/2)+1):
+      if number%i==0:
+        is_prime=False
+        break
+  if is_prime:
+    print(f'{number} is Prime')
+  else:
+    print(f'{number} is not Prime')
+    
+
+prime_checker(number=n)
