cd44efae414f524341a3f9efe06ea093683674db
Mahmood <mahmood.kn1526@gmail.com>
2024-09-02 14:19:21 +0330

    complete part 3 of day 8 challenge

--- day-8/main.py
+++ day-8/main.py
@@ -14,21 +14,64 @@ import math
 # print(f"You'll need {cans} cans of paint.")
 
 # exercise 2 (prime number checker)
-n=int(input('Check this number: '))
+# n=int(input('Check this number: '))
 
-def prime_checker(number):
-  is_prime=True
-  if number== 1:
-    print('1 is neither prime nor composite number.')
-  else:
-    for i in range(2,math.floor(number/2)+1):
-      if number%i==0:
-        is_prime=False
-        break
-  if is_prime:
-    print(f'{number} is Prime')
-  else:
-    print(f'{number} is not Prime')
+# def prime_checker(number):
+#   is_prime=True
+#   if number== 1:
+#     print('1 is neither prime nor composite number.')
+#   else:
+#     for i in range(2,math.floor(number/2)+1):
+#       if number%i==0:
+#         is_prime=False
+#         break
+#   if is_prime:
+#     print(f'{number} is Prime')
+#   else:
+#     print(f'{number} is not Prime')
     
 
-prime_checker(number=n)
+# prime_checker(number=n)
+
+# day project
+#            0    1    2    3    4
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+direction_options=['encode','decode']
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+if not direction in direction_options:
+  print("Wrong input")
+  exit()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(plain_text,shift_amount):
+  encrypted=''
+  for letter in plain_text:
+    if letter in alphabet:
+      index=alphabet.index(letter)
+      if index+shift_amount>=len(alphabet):
+        can_go=len(alphabet)-1 -index
+        should_go_from_begin=shift_amount-can_go-1
+        encrypted+=alphabet[should_go_from_begin]
+        
+      else:
+        encrypted+=alphabet[index+shift_amount]
+    else:
+      encrypted+=letter
+      
+  print(f'The encoded text is: {encrypted}')
+
+def decrypt(plain_text,shift_amount):
+  decrypted=''
+  for letter in plain_text:
+    if  letter in alphabet:
+      index=alphabet.index(letter)
+      decrypted+=alphabet[index-shift_amount]
+    else:
+      decrypted+=letter
+  print(f'The decoded text is: {decrypted}')
+
+if direction=='encode':
+  encrypt(plain_text=text,shift_amount=shift)
+elif direction=='decode':
+  decrypt(plain_text=text,shift_amount=shift)
