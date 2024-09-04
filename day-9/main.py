398a46bdeec9e3f5d12b910a2abb97d55f1c6205
Mahmood <mahmood.kn1526@gmail.com>
2024-09-04 12:49:18 +0330

    complete exercise 2 of day 9

--- day-9/main.py
+++ day-9/main.py
@@ -1,22 +1,55 @@
 # exercise 1
-student_scores = {
-  "Harry": 81,
-  "Ron": 78,
-  "Hermione": 99, 
-  "Draco": 74,
-  "Neville": 62,
-}
+# student_scores = {
+#   "Harry": 81,
+#   "Ron": 78,
+#   "Hermione": 99, 
+#   "Draco": 74,
+#   "Neville": 62,
+# }
 
-student_grades={}
+# student_grades={}
 
-for key in student_scores:
-  if student_scores[key]>90:
-    student_grades[key]="Outstanding"
-  elif student_scores[key]>80 :
-    student_grades[key]="Exceeds Expectations"
-  elif student_scores[key]>70 :
-    student_grades[key]="Acceptable"
-  else:
-    student_grades[key]="Fail"
+# for key in student_scores:
+#   if student_scores[key]>90:
+#     student_grades[key]="Outstanding"
+#   elif student_scores[key]>80 :
+#     student_grades[key]="Exceeds Expectations"
+#   elif student_scores[key]>70 :
+#     student_grades[key]="Acceptable"
+#   else:
+#     student_grades[key]="Fail"
 
-print(student_grades)
+# print(student_grades)
+
+# exercise 1
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+#🚨 Do NOT change the code above
+
+#TODO: Write the function that will allow new countries
+#to be added to the travel_log. 👇
+
+def add_new_country(country,visits,cities):
+  travel_log.append({
+    "country":country,
+    "visits":visits,
+    "cities":cities
+  })
+
+
+
+#🚨 Do not change the code below
+print(travel_log)
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
+
